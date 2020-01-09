1111cf3317b90a0c3ef32d4a69f428a677e7afee
VishnuMatli <matlikinnu@gmail.com>
2020-01-09 19:36:53 +0530

    completed Game

--- js/Game.js
+++ js/Game.js
@@ -44,6 +44,9 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+
+    player.getCarsAtEnd();
+
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -93,6 +96,8 @@ class Game {
 
     if(player.distance > 3860){
       gameState = 2;
+      player.rank += 1;
+      Player.updateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +105,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
